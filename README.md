d63021b368acd3d5cc08aed03696e0612a73fa48
nelak <elakovnikita24@yandex.ru>
2022-05-05 00:37:05 +0300

    add tests and README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+# Automate tests Imgur
+
+### Used Technologies & Tools:
+<p align="left">
+<img height="40" width="40" src="src/images/java-logo.svg" alt="java">
+<img height="40" width="40" src="src/images/junit5-logo.svg" alt="junit5">
+<img height="40" width="40" src="src/images/rest-assured-logo.png" alt="rest-assured">
+<img height="40" width="40" src="src/images/IDEA-logo.svg" alt="IDEA">
+<img height="40" width="40" src="src/images/allure-Report-logo.svg" alt="allure">
+</p>
+
+---
+## API Documentation
+#### https://api.imgur.com/
---
+++ src/main/java/models/GetAccountResponse.java
@@ -0,0 +1,60 @@
+package models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@JsonPropertyOrder({
+        "data",
+        "success",
+        "status"
+})
+
+@NoArgsConstructor
+@Data
+public class GetAccountResponse {
+    @JsonProperty("data")
+    private AccountData data;
+    @JsonProperty("success")
+    private Boolean success;
+    @JsonProperty("status")
+    private Integer status;
+    @Data
+    public static class AccountData {
+
+        @JsonProperty("id")
+        private Integer id;
+        @JsonProperty("url")
+        private String url;
+        @JsonProperty("bio")
+        private String bio;
+        @JsonProperty("avatar")
+        private String avatar;
+        @JsonProperty("avatar_name")
+        private String avatarName;
+        @JsonProperty("cover")
+        private String cover;
+        @JsonProperty("cover_name")
+        private String coverName;
+        @JsonProperty("reputation")
+        private Integer reputation;
+        @JsonProperty("reputation_name")
+        private String reputationName;
+        @JsonProperty("created")
+        private Integer created;
+        @JsonProperty("pro_expiration")
+        private Boolean proExpiration;
+        @JsonProperty("user_follow")
+        private UserFollow userFollow;
+        @JsonProperty("is_blocked")
+        private Boolean isBlocked;
+    }
+
+    @Data
+    public static class UserFollow {
+        @JsonProperty("status")
+        private Boolean status;
+    }
+
+}
